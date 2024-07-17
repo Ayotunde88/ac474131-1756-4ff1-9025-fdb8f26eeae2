2ccea63f44e70f9451a02a88bc585492dd41c3dc
Ayotunde <ayotunde@Ayotundes-MacBook-Pro.local>
2024-07-17 17:29:43 -0400

    update

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -27,7 +27,9 @@ const PhoneMenu = () => {
             <div className='phone-menu'>
                 <div className='row phone-menu-icons'>
                     <div className='col-4'>
-                        <i className="fas fa-phone-alt" style={{ textAlign: "center", }}></i>
+                        <span>
+                            <i className="fas fa-phone-alt" style={{ textAlign: "center", }}></i> <span style={{ color: "#fff", fontSize: "10px", background: "red", marginLeft: "-10px", marginTop: "-15px", borderRadius: "50%", padding: "3px" }}>{activities.length}</span>
+                        </span>
                     </div>
                     <div className='col-4'>
                         <i className="fas fa-user-alt" style={{ textAlign: "center", }}></i>
